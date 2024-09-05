b1f2ab4d1647c1b3ca059e9b0d9bb7092d794380
yesdukext <yves.dukuze.ext@bbc.co.uk>
2024-09-05 15:50:00 +0100

    Initial Commit

---
+++ BrowsePlayableContent/BrowsePlayableContentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrowsePlayableContentApp.swift
+//  BrowsePlayableContent
+//
+//  Created by Yves Dukuze on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BrowsePlayableContentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
